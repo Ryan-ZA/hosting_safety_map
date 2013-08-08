441e97c062bb3b80d95e76ff26a84b8b5f04ee18
RyanZA <ryanza@gmail.com>
2013-08-08 23:04:04 +0200

    Basic map and a bit of test data

---
+++ data.js
@@ -0,0 +1,17 @@
+var safetyData = {
+	"US" : 0,
+	"DE" : 0,
+	"IS" : 2,
+	"GB" : 1,
+	"RU" : 1,
+	"CN" : 1,
+};
+
+var infoData = {
+	"US" : "Multiple cases of forced compliance with a requirement for secrecy.",
+	"DE" : "Germany has laws which force every mail provider to install an access point to the German authorities and intelligence agencies",
+	"IS" : "No rumours as yet?",
+	"GB" : "Rumored to be worse than USA?",
+	"RU" : "Likely a bad bet",
+	"CN" : "Likely a bad bad",
+};
